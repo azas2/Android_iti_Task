3b34acf40d36ac072fdf5c1193cee7a20a3ffb42
ahmed <ahmed6799@gmail.com>
2023-08-05 11:04:45 +0300

    task3 V:1.1

--- app/src/main/java/com/mala/task1/MainActivity.kt
+++ app/src/main/java/com/mala/task1/MainActivity.kt
@@ -6,51 +6,58 @@ import android.widget.Toast
 import com.mala.task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    fun checkfix(check:String):String{
-        if (check=="Female"){
+    fun checkfix(check: String): String {
+        if (check == "Female") {
             return "Ms, "
-        }
-        else
+        } else
             return "Mr, "
+    }
+    fun giveSports(check: String):String{
+        val sports = check.split("\n")
+        val sportsString = StringBuilder()
+        for (sport in sports) {
+            sportsString.append("$sport")
+        }
+        return sportsString.toString()
     }
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val userName=binding.usernameEd.text
-       lateinit var checkSport:String
-        lateinit var checkGender:String
+        val userName = binding.usernameEd.text
 
-       // check of sport
-        binding.Football.setOnCheckedChangeListener{  buttonView, isChecked ->
-            if(isChecked)
-                checkSport="Football"
-        }
-        binding.basketball.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(isChecked)
-                checkSport="basktball"
-        }
-        binding.valleyball.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked)
-                checkSport="valleyball"
-        }
-        binding.Female.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(isChecked)
-                checkGender="Female"
-        }
-        binding.Male.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(isChecked)
-                checkGender="male"
-        }
-
-
+            lateinit var checkGender: String
+            var checkSports: String=""
+            // check of sport
+            binding.Football.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked)
+                    checkSports += "Football \n"
+            }
+            binding.basketball.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked)
+                    checkSports += "basketball \n"
+            }
+            binding.valleyball.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked)
+                    checkSports += "valleyball"
+            }
+            binding.Female.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked)
+                    checkGender = "Female"
+            }
+            binding.Male.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked)
+                    checkGender = "male"
+            }
 
 
-binding.btnLog.setOnClickListener{
-    Toast.makeText(baseContext, "welcom ${checkfix(checkGender)}${userName.toString()},your favourite sport is $checkSport and your Gender is $checkGender ", Toast.LENGTH_LONG).show()
-}
 
 
+            binding.btnLog.setOnClickListener {
+                val sportChose= giveSports(checkSports)
+                val toShow="Welcome ${checkfix(checkGender)} ${userName.toString()}, your favorite sports are:\n$sportChose and your gender is $checkGender"
+                Toast.makeText(this, toShow, Toast.LENGTH_LONG).show()
+            }
     }
 }
